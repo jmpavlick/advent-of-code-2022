b07aae995bc468d9e45a197496512f16504027e5
John Pavlick <jmpavlick@gmail.com>
2022-12-01 10:35:08 -0500

    master: parts 1 and 2 for day one are done

--- src/Day01.elm
+++ src/Day01.elm
@@ -3,7 +3,19 @@ module Day01 exposing (..)
 
 eval : String -> String
 eval input =
-    parse input |> Debug.toString
+    let
+        inputRankedDesc : List Int
+        inputRankedDesc =
+            parse input
+    in
+    String.join " "
+        [ "part 1:"
+        , inputRankedDesc |> List.head |> Maybe.withDefault 0 |> Debug.toString
+        , "; part 2:"
+        , List.take 3 inputRankedDesc
+            |> List.foldl (+) 0
+            |> Debug.toString
+        ]
 
 
 type alias Line =
@@ -29,11 +41,9 @@ step line list =
                 cursedToInt line |> List.singleton
 
 
-parse : String -> Int
+parse : String -> List Int
 parse input =
     String.split "\n" input
         |> List.foldl step []
         |> List.sort
         |> List.reverse
-        |> List.head
-        |> Maybe.withDefault 0

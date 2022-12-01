b92ed6acefc08c69905b8263ae89ab28205a01c6
John Pavlick <jmpavlick@gmail.com>
2022-12-01 08:19:39 -0500

    master: got the loop tight

--- src/Day01.elm
+++ src/Day01.elm
@@ -1,5 +1,17 @@
 module Day01 exposing (..)
 
+
 eval : String -> String
-eval _ =
-    "lol"
+eval input =
+    parse input |> always "todo: finish"
+
+
+type alias Elf =
+    List Int
+
+
+parse : String -> List Elf
+parse input =
+    String.split "\n" input
+        |> Debug.log "split strings"
+        |> always []

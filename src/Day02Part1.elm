430c2dbe260a8a0aed616fdc6eae8197a8cb4e3d
John Pavlick <jmpavlick@gmail.com>
2022-12-02 05:39:15 -0500

    master: savepoint

---
+++ src/Day02Part1.elm
@@ -0,0 +1,143 @@
+module Day02Part1 exposing (..)
+
+
+eval : String -> String
+eval input =
+    parse input
+        |> List.map play
+        |> List.foldl (+) 0
+        |> String.fromInt
+
+
+type Move
+    = Rock
+    | Paper
+    | Scissors
+
+
+type Outcome
+    = Win
+    | Lose
+    | Draw
+
+
+parse : String -> List ( Move, Move )
+parse input =
+    String.split "\n" input
+        |> List.map (String.split " ")
+        |> List.map
+            (\arr ->
+                case arr of
+                    [ elfLetter, playerLetter ] ->
+                        Just ( elfLetter, playerLetter )
+
+                    _ ->
+                        Nothing
+            )
+        |> values
+        |> List.map
+            (\( elfLetter, playerLetter ) ->
+                ( letterToMove elfLetter
+                , letterToMove playerLetter
+                )
+                    |> tupleValues
+            )
+        |> values
+
+
+letterToMove : String -> Maybe Move
+letterToMove input =
+    case input of
+        "A" ->
+            Just Rock
+
+        "B" ->
+            Just Paper
+
+        "C" ->
+            Just Scissors
+
+        "X" ->
+            Just Rock
+
+        "Y" ->
+            Just Paper
+
+        "Z" ->
+            Just Scissors
+
+        _ ->
+            Nothing
+
+
+tupleValues : ( Maybe a, Maybe a ) -> Maybe ( a, a )
+tupleValues ( ma, mb ) =
+    Maybe.map2 (\x y -> ( x, y )) ma mb
+
+
+values : List (Maybe a) -> List a
+values =
+    List.filterMap Basics.identity
+
+
+play : ( Move, Move ) -> Int
+play ( elf, player ) =
+    (winLoseDraw elf player |> outcomeScore)
+        + moveScore player
+
+
+moveScore : Move -> Int
+moveScore move =
+    case move of
+        Rock ->
+            1
+
+        Paper ->
+            2
+
+        Scissors ->
+            3
+
+
+winLoseDraw : Move -> Move -> Outcome
+winLoseDraw elf player =
+    case ( elf, player ) of
+        ( Rock, Paper ) ->
+            Win
+
+        ( Rock, Scissors ) ->
+            Lose
+
+        ( Paper, Rock ) ->
+            Lose
+
+        ( Paper, Scissors ) ->
+            Win
+
+        ( Scissors, Paper ) ->
+            Lose
+
+        ( Scissors, Rock ) ->
+            Win
+
+        ( Rock, Rock ) ->
+            Draw
+
+        ( Paper, Paper ) ->
+            Draw
+
+        ( Scissors, Scissors ) ->
+            Draw
+
+
+outcomeScore : Outcome -> Int
+outcomeScore outcome =
+    case outcome of
+        Win ->
+            6
+
+        Lose ->
+            0
+
+        Draw ->
+            3
--- src/Main.elm
+++ src/Main.elm
@@ -1,29 +1,40 @@
 module Main exposing (..)
 
 import Day01
+import Day02Part1
+
 
 type alias Flags =
     { filename : String
     , input : String
     }
 
-type alias Model = ()
 
-type Msg = Msg
+type alias Model =
+    ()
+
+
+type Msg
+    = Msg
+
 
 main : Program Flags Model Msg
 main =
     Platform.worker
-        { init = \flags -> eval flags |> Debug.log "output" |> always ((), Cmd.none)
-        , update = \_ _ -> ((), Cmd.none)
+        { init = \flags -> eval flags |> Debug.log "output" |> always ( (), Cmd.none )
+        , update = \_ _ -> ( (), Cmd.none )
         , subscriptions = \_ -> Sub.none
         }
+
 
 eval : Flags -> String
 eval { filename, input } =
     case filename of
         "Day01.txt" ->
             Day01.eval input
+
+        "Day02Part1.txt" ->
+            Day02Part1.eval input
 
         _ ->
             String.join " " [ "No matching eval for", filename ]

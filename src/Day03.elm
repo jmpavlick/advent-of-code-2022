65aae2a6de474c1fc2c58f129f160ca15256d310
John Pavlick <jmpavlick@gmail.com>
2022-12-03 00:33:51 -0500

    master: savepoint

--- src/Day03.elm
+++ src/Day03.elm
@@ -19,8 +19,20 @@ part2 =
 
 
 part1 : String -> String
-part1 =
-    always "todo"
+part1 input =
+    Util.lines input
+        -- break input into lines
+        |> List.map compartments
+        -- map each line to compartments
+        |> List.map findMatch
+        -- find the match for each compartment
+        |> Util.values
+        -- List (Maybe a) -> List a
+        |> List.map priorityMap
+        -- map each match to its priority value
+        |> List.foldl (+) 0
+        -- sum them
+        |> String.fromInt
 
 
 {-| 'a' - 'z' are 1 - 26
@@ -44,3 +56,40 @@ priorityMap char =
            else
             upperOffset
           )
+
+
+compartments : String -> ( List Char, List Char )
+compartments input =
+    let
+        chars : List Char
+        chars =
+            String.toList input
+
+        half : Int
+        half =
+            List.length chars
+                |> (\l -> Basics.toFloat l / 2)
+                |> Basics.round
+    in
+    ( List.take half chars
+    , List.drop half chars
+    )
+
+
+findMatch : ( List Char, List Char ) -> Maybe Char
+findMatch ( a, b ) =
+    let
+        step : Char -> Maybe Char -> Maybe Char
+        step current state =
+            case state of
+                Nothing ->
+                    if List.member current a then
+                        Just current
+
+                    else
+                        Nothing
+
+                just ->
+                    just
+    in
+    List.foldl step Nothing b
